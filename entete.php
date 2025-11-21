a30f89e1b7f2c4e298f164850e0f1c72a61f99d4
unknown <sio1@c106-10.220056s.local>
2025-11-21 17:12:53 +0100

    Projet Biblio Drive

---
+++ entete.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p> La Bibliothèque de Moulinsart est fermé au public jusqu'à nouvel ordre.</p>
+	<p> Mais il vous est possible de réserver et retirer vos livres via notre service Biblio Drive</p>
+	<input type="text" name="Auteur" size="20"/>
+	<input type="button" name="btn" value="clic">
+	barre de recherche (include)
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Bibliothèque</title>
+</head>
+<body>
+    <div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-9">
+				<p> La Bibliothèque de Moulinsart est fermé au public jusqu'à nouvel ordre.</p>
+				<p> Mais il vous est possible de réserver et retirer vos livres via notre service Biblio Drive</p>
+				<span class="navbar-text">Navbar text</span>
+				<input type="text" name="Auteur" size="20"/>
+				<input type="button" name="btn" value="clic">
+					barre de recherche (include)
+			</div>
+			<div class="col-sm-3">
+					<img src="Château_de_Moulinsart.jpg" alt="Château_de_Moulinsart" width="460" height="345">
+			</div>
+		</div>
+		<div class="row">
+		   <div class="col-sm-9">
+					carroussel / résultat de la recherche / pages d'admin (ajout d'un livre)
+			</div>
+			<div class="col-sm-3" >
+					formulaire de connexion / profil connecté (include)
+			</div>
+		</div>
+</body>
+</html>
